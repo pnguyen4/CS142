e928954c7433f62ceaf0b39391cde96badf432cc
Phillip Hoang Nguyen <phillip.nguyen@uvm.edu>
2017-09-16 00:20:21 -0400

    add labs 1 and 2 (lab2 has password security change)

---
+++ lab2/header.php
@@ -0,0 +1,10 @@
+<!-- %%%%%%%%%%%%%%%%%%%%%%     Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
+<header>
+    <figure class="float_right small">
+        <img alt="calvin and hobbes has influenced my snowman making." src="images/snowmen.jpg">
+        <figcaption>Snowmen observing an accident.</figcaption>
+    </figure>
+    <h1>Phillip Nguyen Lab 2 - Sample database and php integration</h1>
+    <h2>Try these links out</h2>
+</header>
+<!-- %%%%%%%%%%%%%%%%%%%%% Ends Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
