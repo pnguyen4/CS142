36399811a3554e48f82bb1f6a4c69545ebb57071
Phillip Hoang Nguyen <phillip.nguyen@uvm.edu>
2017-10-06 11:34:11 -0400

    cleanup and small changes

--- Hikedit/header.php
+++ Hikedit/header.php
@@ -1,10 +1,5 @@
 <!-- %%%%%%%%%%%%%%%%%%%%%%     Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
 <header>
-    <figure class="float_right small">
-        <img alt="calvin and hobbes has influenced my snowman making." src="images/snowmen.jpg">
-        <figcaption>Snowmen observing an accident.</figcaption>
-    </figure>
-    <h1>Phillip Nguyen Lab 2 - Sample database and php integration</h1>
-    <h2>Try these links out</h2>
+    <h1>Phillip Nguyen Lab 4 - Hikedit Database</h1>
 </header>
 <!-- %%%%%%%%%%%%%%%%%%%%% Ends Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
--- Hikedit/nav.php
+++ Hikedit/nav.php
@@ -3,37 +3,19 @@
     <ol>
         <?php
         // This sets a class for current page so you can style it differently
-        
+
         print '<li ';
         if ($PATH_PARTS['filename'] == 'index') {
             print ' class="activePage" ';
         }
         print '><a href="index.php">Home</a></li>';
-       
+
         print '<li ';
         if ($PATH_PARTS['filename'] == 'sampleQuery') {
             print ' class="activePage" ';
         }
         print '><a href="sampleQuery.php">Sample SELECT</a></li>';
-       
-        print '<li ';
-        if ($PATH_PARTS['filename'] == 'sampleQuery2') {
-            print ' class="activePage" ';
-        }
-        print '><a href="sampleQuery2.php">Sample INSERT</a></li>';
-        
-        print '<li ';
-        if ($PATH_PARTS['filename'] == 'sampleQuery3') {
-            print ' class="activePage" ';
-        }
-        print '><a href="sampleQuery3.php">Sample SELECT (with ORDER clause)</a></li>';
-        
-        print '<li ';
-        if ($PATH_PARTS['filename'] == 'sampleQuery4') {
-            print ' class="activePage" ';
-        }
-        print '><a href="sampleQuery4.php">Sample test (fails)</a></li>';
-        
+
         print '<li ';
         if ($PATH_PARTS['filename'] == 'tables') {
             print ' class="activePage" ';
