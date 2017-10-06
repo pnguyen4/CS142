de58f69220d2315bb483bcc6e5131ff07b2ebb89
Phillip Hoang Nguyen <phillip.nguyen@uvm.edu>
2017-10-06 12:08:42 -0400

    index lists tblTrails

--- Hikedit/index.php
+++ Hikedit/index.php
@@ -1,15 +1,37 @@
 <?php
-
+include 'top.php';
 //##############################################################################
 //
-// main home page for the site 
-// 
+// This page lists the records based on the query given
+//
 //##############################################################################
-include "top.php";
+$records = '';
 
-// Begin output
-print '<article>';
-print '<h2>Sample Page</h2>';
-print '</article>';
-include "footer.php";
+$query = 'SELECT * FROM tblTrails';
+
+// NOTE: The full method call would be:
+//           $thisDatabaseReader->querySecurityOk($query, 0, 0, 0, 0, 0)
+if ($thisDatabaseReader->querySecurityOk($query, 0)) {
+    $query = $thisDatabaseReader->sanitizeQuery($query);
+    $records = $thisDatabaseReader->select($query, '');
+
+}
+
+if (DEBUG) {
+    print '<p>Contents of the array<pre>';
+    print_r($records);
+    print '</pre></p>';
+}
+
+print '<h2 class="alternateRows">List tblTrails data:</h2>';
+print "<h3>fldTrailName | fldTotalDistance | fldHikingTime | fldVerticalRise | fldRating</h3>";
+if (is_array($records)) {
+    foreach ($records as $record) {
+        //print '<p>' . $record['fldFirstName'] . ' ' . $record['fldLastName'] . '</p>';
+		print '<p>' . $record['fldTrailName']. " " . $record['fldTotalDistance']
+		. " " . $record['fldHikingTime'] . " " . $record['fldVerticalRise'] . " " .
+		$record['fldRating'] . '</p>';
+    }
+}
+include 'footer.php';
 ?>
